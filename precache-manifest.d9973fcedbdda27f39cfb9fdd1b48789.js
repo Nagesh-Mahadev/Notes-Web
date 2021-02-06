2d84a550a3be83bacc06d222c0ffce0bb62fc278
Nagesh M <69685188+Nagesh-Mahadev@users.noreply.github.com>
2021-02-06 15:13:07 +0530

    Updates

---
+++ precache-manifest.d9973fcedbdda27f39cfb9fdd1b48789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c9c7a4f8d81f5b9d0958b362b52a20",
+    "url": "/Notes-Web/index.html"
+  },
+  {
+    "revision": "c41b4c6ecbedb26ff878",
+    "url": "/Notes-Web/static/css/main.34f20c99.chunk.css"
+  },
+  {
+    "revision": "93a6927b6491a9a8448d",
+    "url": "/Notes-Web/static/js/2.57631fa4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Notes-Web/static/js/2.57631fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41b4c6ecbedb26ff878",
+    "url": "/Notes-Web/static/js/main.8fc40242.chunk.js"
+  },
+  {
+    "revision": "04fb7b62d1c36688c73e",
+    "url": "/Notes-Web/static/js/runtime-main.742c8d9e.js"
+  }
+]);
